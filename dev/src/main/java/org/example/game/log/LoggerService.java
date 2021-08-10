8e74900f51b79283394835ad87482129caf5de0d
pj_zhong <pj_zhong@163.com>
2021-08-10 22:35:51 +0800

    【log4j2】 system 手动执行日志位置

--- dev/src/main/java/org/example/game/log/LoggerService.java
+++ dev/src/main/java/org/example/game/log/LoggerService.java
@@ -1,5 +1,9 @@
 package org.example.game.log;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import javax.annotation.PostConstruct;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
@@ -14,6 +18,24 @@ public class LoggerService {
   private Logger sysLogger;
   @Value("${game.id}")
   private String gameId;
+
+  /** 下次时间变化(单位:天) */
+  private long nextRolloverMillis;
+  /** 今天时间，方便获取年月日 */
+  private ZonedDateTime today;
+  /** 缓存builder */
+  private ThreadLocal<StringBuilder> threadLocalSb = ThreadLocal.withInitial(StringBuilder::new);
+
+  public LoggerService() {
+    rollover();
+  }
+
+  private void rollover() {
+    ZonedDateTime dateTime = ZonedDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT,
+        ZoneId.systemDefault());
+    nextRolloverMillis = dateTime.plusDays(1).toInstant().toEpochMilli();
+    today = dateTime;
+  }
 
 
   @PostConstruct
@@ -32,8 +54,33 @@ public class LoggerService {
   }
 
   public void sysLog() {
-    //TODO Is Ok, make it better
-    ThreadContext.put("game_sys_log", gameId + "/sys/test_system");
+    initSysLogContext("test");
     sysLogger.info("asdfasdf");
+  }
+
+  private void initSysLogContext(String sysName) {
+    if (nextRolloverMillis < System.currentTimeMillis()) {
+      rollover();
+    }
+    //TODO Is Ok, make it better
+    StringBuilder sb = threadLocalSb.get();
+    sb.setLength(0);
+
+
+    sb.append(gameId).append("/sys/");
+    fillDataPattern(sb);
+    sb.append('/').append(sysName);
+
+    ThreadContext.put("game_sys_log", sb.toString());
+  }
+
+  /**
+   * @return dataPattern yyyy-MM-dd
+   * @since 2021年08月08日 22:07:41
+   */
+  private StringBuilder fillDataPattern(StringBuilder sb) {
+    char split = '-';
+    return sb.append(today.getYear()).append(split).append(today.getMonthValue()).append(split)
+        .append(today.getDayOfMonth());
   }
 }
