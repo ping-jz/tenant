11fef5e7482382f0e3de987636d3e501f5bcffe7
pj_zhong <pj_zhong@163.com>
2021-09-30 11:38:31 +0800

    【Actor】简化版的Actor，就是个二级队列

---
+++ util/src/main/java/org/example/actor/DefaultActor.java
@@ -0,0 +1,91 @@
+package org.example.actor;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 默认Actor实现
+ * <p>此Actor共有四个状态，运行时会1至3中轮流切换。一旦关闭则不可在开启，关闭时不在接受新的任务，现在有任务会按顺序执行：</p>
+ * <p>1:空闲(等待任务中)</p>
+ * <p>2:等待(在线程任务队列等待调度)</p>
+ * <p>3:执行中(获得控制权，正在执行任务)</p>
+ * <p>4:已关闭</p>
+ *
+ * @author ZJP
+ * @since 2021年09月29日 23:40:00
+ **/
+public class DefaultActor implements Actor {
+
+  public static final int IDLE = 1;
+  public static final int WAITING = 2;
+  public static final int RUNNING = 3;
+  public static final int STOPED = 4;
+
+  /** Actor状态 */
+  private final AtomicInteger state;
+  /** 任务队列 */
+  private final Queue<Runnable> queue;
+  /** 线程池(推荐使用{@link java.util.concurrent.ForkJoinPool}) */
+  private final ExecutorService service;
+
+  public DefaultActor(ExecutorService service) {
+    state = new AtomicInteger(IDLE);
+    queue = new ConcurrentLinkedQueue<>();
+    this.service = service;
+  }
+
+  @Override
+  public void run() {
+    boolean failed = !state.compareAndSet(WAITING, RUNNING);
+    if (failed) {
+      return;
+    }
+
+    int size = ThreadLocalRandom.current().nextInt(1, Short.MAX_VALUE);
+    for (int i = 0; i < size; i++) {
+      try {
+        Runnable runnable = queue.poll();
+        if (runnable == null) {
+          break;
+        }
+
+        runnable.run();
+      } catch (Exception e) {
+        //TODO log me
+      }
+    }
+    state.compareAndSet(RUNNING, IDLE);
+    if (queue.peek() != null) {
+      schedul();
+    }
+  }
+
+  @Override
+  public boolean offer(Runnable runnable) {
+    if (isStoped()) {
+      return false;
+    }
+
+    boolean res = queue.add(runnable);
+    schedul();
+    return res;
+  }
+
+  public boolean isStoped() {
+    return state.get() == STOPED;
+  }
+
+  protected void schedul() {
+    try {
+      if (state.compareAndSet(IDLE, WAITING)) {
+        service.execute(this);
+      }
+    } catch (Exception e) {
+      state.compareAndSet(WAITING, IDLE);
+      //TODO Log me
+    }
+  }
+}
