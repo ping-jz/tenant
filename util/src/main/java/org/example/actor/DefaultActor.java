5222502103a5d5ccc469fe3b9232c0fbf366f8cb
pj_zhong <pj_zhong@163.com>
2021-12-06 18:15:58 +0800

    【二级队列】命名更正

--- util/src/main/java/org/example/actor/DefaultActor.java
+++ util/src/main/java/org/example/actor/DefaultActor.java
@@ -8,11 +8,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 默认Actor实现
- * <p>此Actor共有四个状态，运行时会1至3中轮流切换。一旦关闭则不可在开启，关闭时不在接受新的任务，现在有任务会按顺序执行：</p>
+ * <p>此Actor共有3个状态，运行时会1至3中轮流切换。一旦关闭则不可在开启，关闭时不在接受新的任务，现在有任务会按顺序执行：</p>
  * <p>1:空闲(等待任务中)</p>
  * <p>2:等待(在线程任务队列等待调度)</p>
  * <p>3:执行中(获得控制权，正在执行任务)</p>
- * <p>4:已关闭</p>
  *
  * @author ZJP
  * @since 2021年09月29日 23:40:00
@@ -22,7 +21,6 @@ public class DefaultActor implements Actor {
   public static final int IDLE = 1;
   public static final int WAITING = 2;
   public static final int RUNNING = 3;
-  public static final int STOPED = 4;
 
   /** Actor状态 */
   private final AtomicInteger state;
@@ -30,6 +28,8 @@ public class DefaultActor implements Actor {
   private final Queue<Runnable> queue;
   /** 线程池(推荐使用{@link java.util.concurrent.ForkJoinPool}) */
   private final ExecutorService service;
+  /** 是否停止 */
+  private boolean stopped;
 
   public DefaultActor(ExecutorService service) {
     state = new AtomicInteger(IDLE);
@@ -59,26 +59,30 @@ public class DefaultActor implements Actor {
     }
     state.compareAndSet(RUNNING, IDLE);
     if (queue.peek() != null) {
-      schedul();
+      exec();
     }
   }
 
   @Override
   public boolean offer(Runnable runnable) {
-    if (isStoped()) {
+    if (isStopped()) {
       return false;
     }
 
     boolean res = queue.add(runnable);
-    schedul();
+    exec();
     return res;
   }
 
-  public boolean isStoped() {
-    return state.get() == STOPED;
+  public boolean isStopped() {
+    return stopped;
   }
 
-  protected void schedul() {
+  public void stopped() {
+    stopped = true;
+  }
+
+  protected void exec() {
     try {
       if (state.compareAndSet(IDLE, WAITING)) {
         service.execute(this);
