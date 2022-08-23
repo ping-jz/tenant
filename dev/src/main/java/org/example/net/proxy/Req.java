fe35d5b979fca41e937a670deee96cef68a1201d
pj_zhong <pj_zhong@163.com>
2022-08-23 11:22:31 +0800

    【RPC】简化请求代理的获取和使用方式

---
+++ dev/src/main/java/org/example/net/proxy/Req.java
@@ -0,0 +1,15 @@
+package org.example.net.proxy;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * RPC代理自动注入
+ *
+ * @author ZJP
+ * @since 2022年08月22日 16:15:07
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Req {
+
+}
